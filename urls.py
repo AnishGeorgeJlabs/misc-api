4654225c7ff87fc0ab43f70c66deed92a5f48210
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-08-31 18:35:30 +0530

    base apis for jabong

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', api.test, name='test'),
 
-    url(r'^jabong/test_insert$', api.test_insert, name='test_insert'),
+    url(r'^jabong/test_insert$', jabong.test_insert, name='test_insert'),
     url(r'^jabong/form$', jabong.get_form, name='jabong.form'),
     url(r'^jabong/post$', jabong.post_form, name='jabong.post')
 )
